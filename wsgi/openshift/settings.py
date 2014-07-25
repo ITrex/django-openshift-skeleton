8c8e82aa7cc32f0f38ad8867ff98e7de833e509b
Renat Galimov <renat2017@gmail.com>
2014-07-25 16:16:54 +0400

    Media files serving fixed

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -42,13 +42,8 @@ else:
     }
 
 
-MEDIA_ROOT = os.path.join(
-    os.environ['OPENSHIFT_DATA_DIR'], 'media')
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
+MEDIA_ROOT = os.path.join( os.environ['OPENSHIFT_DATA_DIR'], 'media')
+STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
 
 # Make a dictionary of default keys
 default_keys = { 'SECRET_KEY': 'vm4rl5*ymb@2&d_(gc$gb-^twq9w(u69hi--%$5xrh!xk(t%hw' }
